e7def03fb9a4d3d2476d1b7ff6d3cc8e917bc124
lateofrederick <lateofrederick@gmail.com>
2022-02-16 12:09:14 +0000

    set url paths

--- evolve_transfer/urls.py
+++ evolve_transfer/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from evolve_transfer.views import homepage_view
+from evolve_transfer.views import homepage_view, login_view, register_view
 
 urlpatterns = [
     path('', homepage_view.homepage, name='homepage'),
+    path('login/', login_view.login_request, name='login_request'),
+    path('signup/', register_view.register_request, name='register_request')
 ]
